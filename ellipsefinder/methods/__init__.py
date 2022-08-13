b8c254f46303a6c070ae0adabe9f2e69ad492351
marcus-k <81660172+marcus-k@users.noreply.github.com>
2022-08-12 19:14:28 -0600

    Update error message

--- ellipsefinder/methods/__init__.py
+++ ellipsefinder/methods/__init__.py
@@ -7,7 +7,7 @@ from .finder import EllipseFinder
 try:
     from .AAMED import AAMED
 except ImportError as e:
-    print("Error importing AAMED (does not exist or invalid format). Continuing without AAMED.")
+    print("Error importing AAMED (does not exist/invalid format/version mismatch). Continuing without AAMED.")
 
 from .basic import Basic
 from .canny import Canny
